7105b00ad460cae6e4d39fdab55745944d5893b1
stealthyoda <optiondeals@gmail.com>
2022-04-27 15:08:25 -0400

    This is a test of moving files from staging to LOCAL master/main repository.

---
+++ Demo/PythonFile1.py
@@ -0,0 +1,7 @@
+#This is a test of creating a repository from my local computer and this will be a file within my Demo Directory
+
+print ("Hello World")
+print (2**8)
+print (5*5)
+print (4*2)
+print ((5**2) + (3**3))
